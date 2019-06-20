351bbd2a90a1c6b9dda45ca440ca0ca81545242a
Manisha Patel <ma9shap.gmail.com>
2019-06-20 12:22:26 +0100

    installed express handlebar

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const path = require('path')
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.engine('html', exphbs());
+app.set('views', path.join(__dirname, './views' ))
+// app.engine('html', exphbs({defaultLayout: 'index', extname: '.html'}));
+app.set('view engine', 'html');
+
 const port = process.env.PORT || 8080;
 const mongoose = require('mongoose');
-require('./server/models/users')
-require('./server/models/posts')
+
+require('./server/models/users');
+require('./server/models/posts');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/acebook', {useNewUrlParser: true});
 
@@ -34,13 +43,17 @@ db.once('open', function() {
 
 });
 
-const collection = db.collection('users')
-const collection2 = db.collection('posts')
+const collection = db.collection('users');
+const collection2 = db.collection('posts'); 
 
-app.get('/', async function (req, res) {
+app.get('/test', async function (req, res) {
   const documents = await collection.find().toArray()
   console.log(documents);
   const documents2 = await collection2.find().toArray()
   console.log(documents2);
   res.send(documents2);
-})
+});
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
