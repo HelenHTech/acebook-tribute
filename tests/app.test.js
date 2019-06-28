c788874d863ab21eb106f00c10d7a17b7923d750
Melanie Rogan <melanie.rogan@ft.com>
2019-06-28 10:40:41 +0100

    added more tests and added delete post functionality

--- tests/app.test.js
+++ tests/app.test.js
@@ -19,6 +19,30 @@ describe('acebook posts page', () => {
   });
 });
 
+describe('signup page', () => {
+  test('shows posts page', () => {
+    return request(app)
+    .get('/sign-up')
+    .expect(200)
+  });
+});
+
+describe('acebook login page', () => {
+  test('shows log in', () => {
+    return request(app)
+    .get('/login')
+    .expect(200)
+  });
+});
+
+describe('edit posts', () => {
+  test('shows one post to edit', () => {
+    return request(app)
+    .get('/post-edit')
+    .expect(200)
+  });
+});
+
 afterAll(() => {
 server.close();
 });
